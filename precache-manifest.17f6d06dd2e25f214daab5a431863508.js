fc72caf406007931e5bbeae138da6e25439bc279
Asif Ridwan <asifridwan@gmail.com>
2021-12-18 23:40:15 +0600

    Updates

---
+++ precache-manifest.17f6d06dd2e25f214daab5a431863508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2980e7be739e3760046caeacaaf72c9c",
+    "url": "/ar-budget-tracker/index.html"
+  },
+  {
+    "revision": "e1c0befab27426814b57",
+    "url": "/ar-budget-tracker/static/css/main.fc8bb386.chunk.css"
+  },
+  {
+    "revision": "8cca1b47ecd76b922ea4",
+    "url": "/ar-budget-tracker/static/js/2.b2cd413c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ar-budget-tracker/static/js/2.b2cd413c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c0befab27426814b57",
+    "url": "/ar-budget-tracker/static/js/main.6e98952f.chunk.js"
+  },
+  {
+    "revision": "fe67f04641de2c0d31b6",
+    "url": "/ar-budget-tracker/static/js/runtime-main.0e4a69bc.js"
+  }
+]);
